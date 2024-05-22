93daeb8817d64f3253397d3c61c2fb84b81681ed
Olayinka Ogunwemimo <mimoyinka@gmail.com>
2024-05-22 14:51:09 +0100

    chore add PhotosContainer to page/index

---
+++ src/components/PhotosContainer.tsx
@@ -0,0 +1,23 @@
+import { useState, useCallback } from 'react'
+import usePhotos, { fetchPhotos } from '@/hooks/usePhotos'
+import PhotoList from '@/components/PhotoList'
+import SearchForm from '@/components/SearchForm'
+import KeywordProvider from '@/components/KeywordProvider'
+
+const PhotosContainer = () => {
+    const [page, setPage] = useState(1)
+    const { data, isLoading } = usePhotos(page)
+
+    const loadMore = useCallback(async () => fetchPhotos(page + 1), [])
+
+    if (isLoading) return 'Loading...'
+    return (
+        <KeywordProvider>
+            <SearchForm recentCount={5} type="search" />
+            <br />
+            <PhotoList data={data} onNext={loadMore} />
+        </KeywordProvider>
+    )
+}
+
+export default PhotosContainer
