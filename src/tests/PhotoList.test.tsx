ac6698f9a3e1d9ae0b60b2c22790194230cb7450
Olayinka Ogunwemimo <mimoyinka@gmail.com>
2024-05-22 20:17:50 +0100

    add better tests

--- src/tests/PhotoList.test.tsx
+++ src/tests/PhotoList.test.tsx
@@ -2,12 +2,53 @@ import { render, screen } from '@testing-library/react'
 import PhotoList from '../components/PhotoList.tsx'
 import KeywordProvider from '../components/KeywordProvider.tsx'
 
-describe('Photo', () => {
-    it('should render PhotoList', () => {
-        render(
+beforeEach(() => {
+    // IntersectionObserver isn't available in test environment
+    const mockIntersectionObserver = jest.fn()
+    mockIntersectionObserver.mockReturnValue({
+        observe: () => null,
+        unobserve: () => null,
+        disconnect: () => null,
+    })
+    window.IntersectionObserver = mockIntersectionObserver
+})
+
+describe('PhotoList', () => {
+    it('should render no photos when list is empty', () => {
+        const { container } = render(
             <KeywordProvider>
-                <PhotoList />
+                <PhotoList data={[]} />
             </KeywordProvider>
         )
+        expect(container).toBeEmptyDOMElement()
+    })
+
+    it('should render a list of photos', () => {
+        const photos: PhotoProps[] = [
+            {
+                albumId: 1,
+                id: 3,
+                title: 'officia porro iure quia iusto qui ipsa ut modi',
+                url: 'https://via.placeholder.com/600/24f355',
+                thumbnailUrl: 'https://via.placeholder.com/150/24f355',
+            },
+            {
+                albumId: 1,
+                id: 4,
+                title: 'culpa odio esse rerum omnis laboriosam voluptate repudiandae',
+                url: 'https://via.placeholder.com/600/d32776',
+                thumbnailUrl: 'https://via.placeholder.com/150/d32776',
+            },
+        ]
+        render(
+            <KeywordProvider>
+                <PhotoList data={photos} />
+            </KeywordProvider>
+        )
+        const images = screen.getAllByRole('img')
+        expect(images).toHaveLength(2)
+        photos.forEach((photo, index) => {
+            expect(images[index]).toHaveAttribute('src', photo.url)
+        })
     })
 })
